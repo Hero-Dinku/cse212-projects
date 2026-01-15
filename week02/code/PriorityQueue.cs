4433396f41ba369ba3b56994e8c297f78b3f54f6
AsmamawDinku <adinku@byupathway.edu>
2026-01-14 20:17:30 -0500

    Complete W02 Queues assignment - All files created and fixed

---
+++ week02/code/PriorityQueue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PriorityQueue
+{
+    private List<PriorityItem> _queue = new();
+
+    /// <summary>
+    /// Add a new value to the queue with an associated priority.  The
+    /// node is always added to the back of the queue regardless of 
+    /// the priority.
+    /// </summary>
+    /// <param name=\"value\">The value</param>
+    /// <param name=\"priority\">The priority</param>
+    public void Enqueue(string value, int priority)
+    {
+        var newNode = new PriorityItem(value, priority);
+        _queue.Add(newNode);
+    }
+
+    public string Dequeue()
+    {
+        if (_queue.Count == 0)
+        {
+            throw new InvalidOperationException(\"The queue is empty.\");
+        }
+
+        // Find the index of the item with the highest priority
+        var highPriorityIndex = 0;
+        for (int index = 1; index < _queue.Count; index++)
+        {
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
+            {
+                highPriorityIndex = index;
+            }
+            // If same priority, keep the earliest one (FIFO)
+        }
+
+        // Remove and return the item with the highest priority
+        var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
+        return value;
+    }
+
+    // DO NOT MODIFY THE CODE IN THIS METHOD
+    public override string ToString()
+    {
+        return $\"[{string.Join(\", \", _queue)}]\";
+    }
+}
+
+internal class PriorityItem
+{
+    internal string Value { get; set; }
+    internal int Priority { get; set; }
+
+    internal PriorityItem(string value, int priority)
+    {
+        Value = value;
+        Priority = priority;
+    }
+
+    // DO NOT MODIFY THE CODE IN THIS METHOD
+    public override string ToString()
+    {
+        return $\"{Value} (Pri:{Priority})\";
+    }
+}
---
+++ week02/code/TakingTurnsQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TakingTurnsQueue
+{
+    private readonly Queue<Person> _people = new Queue<Person>();
+
+    public int Length => _people.Count;
+
+    /// <summary>
+    /// Add a person to the queue with the given number of turns
+    /// </summary>
+    /// <param name=\"name\">Name of the person</param>
+    /// <param name=\"turns\">Number of turns (0 or less means infinite)</param>
+    public void AddPerson(string name, int turns)
+    {
+        var person = new Person(name, turns);
+        _people.Enqueue(person);
+    }
+
+    /// <summary>
+    /// Get the next person in the queue
+    /// </summary>
+    /// <returns>The next person</returns>
+    public Person GetNextPerson()
+    {
+        if (_people.Count == 0)
+        {
+            throw new InvalidOperationException(\"No one in the queue.\");
+        }
+
+        var person = _people.Dequeue();
+        
+        // If person has turns left (or infinite turns), add them back
+        if (person.Turns > 1 || person.Turns <= 0)
+        {
+            // Decrease turns if not infinite
+            if (person.Turns > 0)
+            {
+                person.Turns--;
+            }
+            _people.Enqueue(person);
+        }
+        // If person.Turns == 1, they used their last turn, don't add back
+        
+        return person;
+    }
+
+    public override string ToString()
+    {
+        return $\"[{string.Join(\", \", _people)}]\";
+    }
+}
