db2976d5f63b5a2be6c7793b7ea0eeb2fd217aac
AsmamawDinku <adinku@byupathway.edu>
2026-01-29 21:59:08 -0500

    W03 Code: Sets and Maps - Complete implementation of all 5 problems
    - Implemented FindPairs using HashSet for O(n) symmetric pair finding
    - Implemented SummarizeDegrees with dictionary for degree counts
    - Implemented IsAnagram with character counting dictionary
    - Completed Maze class with movement methods and wall checking
    - Created FeatureCollection classes for JSON deserialization
    - Updated EarthquakeDailySummary to extract place and magnitude data
    - All methods pass provided tests and meet efficiency requirements

--- week02/code/obj/Debug/net8.0/code.AssemblyInfo.cs
+++ week02/code/obj/Debug/net8.0/code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef46587d73ff361c92449dd34a6049dc53670310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1fa8470ce581edac7fc2f50b69b45213303c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
